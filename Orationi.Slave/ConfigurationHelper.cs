c34074ef46d91d46662a0ec6a927abfe5cd7b723
Dmitry Pelevin <dpelevin@navicons.ru>
2016-03-20 22:25:58 +0300

    Add configuration manager

---
+++ Orationi.Slave/ConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orationi.CommunicationCore.Model;
+
+namespace Orationi.Slave
+{
+    public class ConfigurationHelper
+    {
+        SlaveConfiguration LoadConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+
+        void SaveConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
